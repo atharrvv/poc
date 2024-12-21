dfe378c5ce289eae06a65f0afaaf1da0e40c84f7
Atharv <105142090+atharrvv@users.noreply.github.com>
2024-12-21 15:54:22 +0530

    Create ip.sh

---
+++ frontend/ip.sh
@@ -0,0 +1,3 @@
+sleep 20
+EXTERNAL_IP=$(kubectl get svc --output=jsonpath='{.items[?(@.status.loadBalancer.ingress)].status.loadBalancer.ingress[0].ip}')
+echo "REACT_APP_API_BASE_URL=http://$EXTERNAL_IP/" > .env
