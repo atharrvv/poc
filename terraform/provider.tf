c80df31d7ff5baeee08bf2ba4a0715efb086f466
Atharv <105142090+atharrvv@users.noreply.github.com>
2025-01-06 15:23:34 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,7 +1,7 @@
 provider "azurerm" {
-  subscription_id = ""
-  client_id       = ""
-  client_secret   = ""
-  tenant_id       = ""
+  subscription_id = "SUBSCRIPTION_ID"
+  client_id       = "CLIENT_ID"
+  client_secret   = "CLIENT_SECRET"
+  tenant_id       = "TENANT_ID"
   features {}
 }
