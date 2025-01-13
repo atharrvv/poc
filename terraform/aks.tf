af3dcad94f831529f0356e779e5d6398a4abc5d0
Atharv <105142090+atharrvv@users.noreply.github.com>
2025-01-13 17:30:25 +0530

    Update aks.tf

--- terraform/aks.tf
+++ terraform/aks.tf
@@ -1,12 +1,13 @@
 resource "azurerm_kubernetes_cluster" "aks" {
   name                = "dilli"
+  kubernetes_version  = "1.29.2"
   location            = azurerm_resource_group.resource_group.location
   resource_group_name = azurerm_resource_group.resource_group.name
   dns_prefix          = "safar"
 
   default_node_pool {
     name       = "default"
-    node_count = 2
+    node_count = 1
     vm_size    = "Standard_D2s_v3"
   }
 
@@ -18,6 +19,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
     Environment = "Production"
   }
 }
+
 
 output "client_certificate" {
   value     = azurerm_kubernetes_cluster.aks.kube_config[0].client_certificate
